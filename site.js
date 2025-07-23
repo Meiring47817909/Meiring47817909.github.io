7e424fa842dbf167cafefbf67f4096b4d84dc893
Meiring van Niekerk <47817909@mynwu.ac.za>
2025-07-23 22:36:51 +0200

    Virtual CV (portfolio website)

---
+++ site.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar");
+    const toggleButton = document.getElementById("toggleSidebar");
+
+    toggleButton.addEventListener("click", function () {
+        sidebar.classList.toggle("expanded");
+    });
+});
