75eb9b6d507d4cae9086c39d6e2b23f89122256c
Meiring47817909 <47817909@mynwu.ac.za>
2025-07-26 02:06:45 +0200

    Fixed background image

--- site.js
+++ site.js
@@ -6,3 +6,21 @@
         sidebar.classList.toggle("expanded");
     });
 });
+
+window.onload = function(){
+    const tabMe = document.getElementById("me");
+    if (tabMe.classList.contains("active"))
+        addBackground()
+    else
+        removeBackground()
+};
+
+function addBackground() {
+    const page = document.getElementById("pageBackground");
+    page.classList.add("background");
+}
+
+function removeBackground() {
+    const page = document.getElementById("pageBackground");
+    page.classList.remove("background");
+}
